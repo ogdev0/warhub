28843f06eb3a7d10f82ff9f060917f4d9ce92365
ogdev0 <102938546+ogdev0@users.noreply.github.com>
2022-09-25 00:32:10 -0700

    Add files via upload

---
+++ Speed Race Clicker.lua
@@ -0,0 +1,47 @@
+-- https://v3rmillion.net/showthread.php?tid=1023761
+
+getgenv().AutoRebirth = false
+
+
+
+
+function DoRebirth()
+    spawn (function()
+        while getgenv().AutoRebirth == true do
+            --Script start
+            local args = {
+                [1] = game:GetService("Players").LocalPlayer
+            }
+            
+            game:GetService("ReplicatedStorage").ServerMsg.Reborn:InvokeServer(unpack(args))
+            
+            --Script end
+            wait()
+        end
+    end)
+end
+
+
+
+
+local UILibrary = loadstring(game:HttpGet("https://pastebin.com/raw/V1ca2q9s"))()
+local MainUI = UILibrary.Load("Wars ScriptHub")
+local FirstPage = MainUI.AddPage("Home")
+
+local FirstLabel = FirstPage.AddLabel("Owner : war#7642")
+
+
+
+
+local FirstToggle = FirstPage.AddToggle("AutoRebirth", false, function(bool)
+    getgenv().AutoRebirth = bool
+   if bool then 
+    DoRebirth();
+    end
+end)
+
+
+local FirstSlider = FirstPage.AddSlider("Speed ", {Min = 0, Max = 999999999999, Def = 50}, function(bool)
+
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = bool 
+end)

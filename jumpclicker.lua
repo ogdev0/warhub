ecb717b8e014f868369e609a48ece45dd24167ad
ogdev0 <102938546+ogdev0@users.noreply.github.com>
2022-09-26 11:16:04 -0700

    Add files via upload

---
+++ jumpclicker.lua
@@ -0,0 +1,177 @@
+-- autoclick
+game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.7").knit.Services.JumpService.RE.Tap:FireServer()
+--break lb
+local args = {[1] = "Players.aa_TheNoob.PlayerScripts.Client.Controllers.UI.Core.ProgressBarUI:156: attempt to index nil " .."with 'Position'",[2] = "",[3] = "Players.aa_TheNoob.PlayerScripts.Client.Client"}
+game:GetService("ReplicatedStorage").GameAnalyticsError:FireServer(unpack(args))
+
+
+
+
+-- https://v3rmillion.net/showthread.php?tid=1023761
+
+getgenv().AutoClicker = false
+getgenv().AutoWin1 = false
+
+function doTap()
+    spawn (function()
+        while getgenv().AutoClicker == true do
+            --Script start
+            game:GetService("ReplicatedStorage").Packages._Index:FindFirstChild("sleitnick_knit@1.4.7").knit.Services.JumpService.RE.Tap:FireServer()
+
+            --Script end
+            wait(.1)
+        end
+    end)
+end
+
+
+function DoWin1()
+    spawn (function()
+        while getgenv().AutoWin1 == true do
+            --Script start
+            for k,v in next, game:GetService('Players'):GetChildren() do 
+                oghead = v.Character:FindFirstChild('Head')
+                if oghead then 
+                oghead.Size = Vector3.new(2, 2, 2) 
+                game.Players.LocalPlayer.Character.Head.Size = Vector3.new(2, 1, 1)
+                end
+                end
+                delay = 0
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["2"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["3"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["4"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["5"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["6"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["7"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["8"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["9"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["10"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["11"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["12"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["13"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["14"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["15"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["16"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["17"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["18"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["19"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["20"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["21"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["22"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["23"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["24"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["25"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["26"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["27"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["28"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["29"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["30"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["31"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["32"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["33"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["34"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["35"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["36"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["37"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["38"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["39"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["40"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["41"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["42"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["43"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["44"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["45"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["46"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["47"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["48"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["49"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["50"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["51"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["52"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").ScriptableObjects.Heights["53"].Template.CFrame
+                wait(delay)
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-5, 1513783, 46)
+                wait(delay)
+                game.Players.LocalPlayer.Character.Humanoid.Health = 0
+            --Script end
+            wait()
+        end
+    end)
+end
+
+local UILibrary = loadstring(game:HttpGet("https://pastebin.com/raw/V1ca2q9s"))()
+local MainUI = UILibrary.Load("Wars ScriptHub")
+local FirstPage = MainUI.AddPage("Home")
+
+local FirstLabel = FirstPage.AddLabel("Owner : war#7642")
+
+
+
+local FirstToggle = FirstPage.AddToggle("AutoClick", false, function(bool)
+    getgenv().AutoClicker = bool
+   if bool then 
+    doTap();
+    end
+end)
+
+
+local FirstToggle = FirstPage.AddToggle("AutoWin", false, function(Value)
+    getgenv().AutoWin1 = Value
+   if Value then 
+    DoWin1();
+    end
+end)
+
+
+
